7bfd576c3430ee06d133494bb316c523d72d86b8
Felix Scherz <felix.scherz@exxeta.com>
2023-04-02 23:12:06 +0200

    add integration tests

---
+++ tests/integration_tests.rs
@@ -0,0 +1,16 @@
+use std::{
+    fs::{self, read_dir},
+    path::Path,
+};
+
+#[test]
+fn clear_data() {
+    let path = Path::new("data");
+    dbg!(path.read_dir().unwrap().count());
+    for file in read_dir(path).unwrap() {
+        let path = file.unwrap().path();
+        if path.extension().is_some() {
+            fs::remove_file(&path).ok();
+        }
+    }
+}
