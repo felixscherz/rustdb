7b81672ec4e5ed66083164690bac46709fa65c9d
Felix Scherz <felix.scherz@exxeta.com>
2023-05-02 16:03:38 +0200

    track byte offset on data file

--- src/sstable/data.rs
+++ src/sstable/data.rs
@@ -7,6 +7,7 @@ use std::{fs::File, io::BufWriter, io::Read, io::Write};
 pub struct Data {
     pub path: PathBuf,
     file: BufWriter<File>,
+    offset: usize,
 }
 
 pub struct DataIterator {
@@ -25,7 +26,53 @@ impl Iterator for DataIterator {
     type Item = Entry;
 
     fn next(&mut self) -> Option<Entry> {
-        Entry::read(&mut self.reader)
+        // reads the sequence of bytes corresponding to an SSTableEntry from the file
+        let mut len_buffer = [0; 8]; // key length is encoded in first 8 bytes
+        if self.reader.read_exact(&mut len_buffer).is_err() {
+            return None;
+        }
+        let key_len = usize::from_le_bytes(len_buffer); // turn bytes into usize to get key length
+        let mut bool_buffer = [0; 1];
+        // read next byte to get tombstone byte
+        if self.reader.read_exact(&mut bool_buffer).is_err() {
+            return None;
+        }
+        let deleted = bool_buffer[0] != 0;
+        let mut key = vec![0; key_len];
+        let mut value: Option<Vec<u8>> = None;
+        if deleted {
+            // if deleted, then value_len and value don't exist -> next bytes are key bytes
+            if self.reader.read_exact(&mut key).is_err() {
+                return None;
+            }
+        } else {
+            // read the next 8 bytes to get value length as bytes
+            if self.reader.read_exact(&mut len_buffer).is_err() {
+                return None;
+            }
+            let value_len = usize::from_le_bytes(len_buffer);
+            // read next key_len bytes to get key
+            if self.reader.read_exact(&mut key).is_err() {
+                return None;
+            }
+            let mut value_buf = vec![0; value_len];
+            // read next value_len bytes to get value
+            if self.reader.read_exact(&mut value_buf).is_err() {
+                return None;
+            }
+            value = Some(value_buf)
+        }
+        let mut timestamp_buffer = [0; 16];
+        if self.reader.read_exact(&mut timestamp_buffer).is_err() {
+            return None;
+        }
+        let timestamp = u128::from_le_bytes(timestamp_buffer);
+        Some(Entry {
+            key,
+            value,
+            timestamp,
+            deleted,
+        })
     }
 }
 
@@ -36,15 +83,42 @@ impl Data {
 
     pub fn from_path(path: &Path) -> io::Result<Data> {
         let file = OpenOptions::new().append(true).create(true).open(&path)?;
+        let offset: usize = file.metadata().unwrap().len().try_into().unwrap();
         let file = BufWriter::new(file);
         Ok(Data {
             path: path.to_owned(),
             file,
+            offset,
         })
     }
     pub fn write(&mut self, entry: &Entry) -> io::Result<()> {
-        entry.write(&mut self.file)?;
+        self.file.write_all(&entry.key.len().to_le_bytes())?;
+        self.file.write_all(&(entry.deleted as u8).to_le_bytes())?;
+        if let Some(val) = &entry.value {
+            self.file.write_all(&val.len().to_le_bytes())?;
+            self.file.write_all(&entry.key)?;
+            self.file.write_all(&val)?;
+        } else {
+            self.file.write_all(&entry.key)?;
+        }
+        self.file.write_all(&entry.timestamp.to_le_bytes())?;
+        self.offset += Self::size_of_entry(entry);
         Ok(())
+    }
+
+    fn size_of_entry(entry: &Entry) -> usize {
+        let key_size = entry.key.len() + std::mem::size_of::<usize>();
+        let value_size = match &entry.value {
+            Some(val) => val.len() + std::mem::size_of::<usize>(),
+            None => 0,
+        };
+        let deleted_size = std::mem::size_of::<bool>();
+        let timestamp_size = std::mem::size_of::<u128>();
+        key_size + value_size + deleted_size + timestamp_size
+    }
+
+    pub fn get_offset(&self) -> usize {
+        self.offset
     }
 
     pub fn flush(&mut self) -> io::Result<()> {
@@ -60,72 +134,6 @@ impl Data {
             }
         }
         Ok(None)
-    }
-}
-
-impl Entry {
-    pub fn write(&self, file: &mut BufWriter<File>) -> io::Result<()> {
-        file.write_all(&self.key.len().to_le_bytes())?;
-        file.write_all(&(self.deleted as u8).to_le_bytes())?;
-        if let Some(val) = &self.value {
-            file.write_all(&val.len().to_le_bytes())?;
-            file.write_all(&self.key)?;
-            file.write_all(&val)?;
-        } else {
-            file.write_all(&self.key)?;
-        }
-        file.write_all(&self.timestamp.to_le_bytes())?;
-        Ok(())
-    }
-
-    pub fn read(file: &mut BufReader<File>) -> Option<Entry> {
-        // reads the sequence of bytes corresponding to an SSTableEntry from the file
-        let mut len_buffer = [0; 8]; // key length is encoded in first 8 bytes
-        if file.read_exact(&mut len_buffer).is_err() {
-            return None;
-        }
-        let key_len = usize::from_le_bytes(len_buffer); // turn bytes into usize to get key length
-        let mut bool_buffer = [0; 1];
-        // read next byte to get tombstone byte
-        if file.read_exact(&mut bool_buffer).is_err() {
-            return None;
-        }
-        let deleted = bool_buffer[0] != 0;
-        let mut key = vec![0; key_len];
-        let mut value: Option<Vec<u8>> = None;
-        if deleted {
-            // if deleted, then value_len and value don't exist -> next bytes are key bytes
-            if file.read_exact(&mut key).is_err() {
-                return None;
-            }
-        } else {
-            // read the next 8 bytes to get value length as bytes
-            if file.read_exact(&mut len_buffer).is_err() {
-                return None;
-            }
-            let value_len = usize::from_le_bytes(len_buffer);
-            // read next key_len bytes to get key
-            if file.read_exact(&mut key).is_err() {
-                return None;
-            }
-            let mut value_buf = vec![0; value_len];
-            // read next value_len bytes to get value
-            if file.read_exact(&mut value_buf).is_err() {
-                return None;
-            }
-            value = Some(value_buf)
-        }
-        let mut timestamp_buffer = [0; 16];
-        if file.read_exact(&mut timestamp_buffer).is_err() {
-            return None;
-        }
-        let timestamp = u128::from_le_bytes(timestamp_buffer);
-        Some(Entry {
-            key,
-            value,
-            timestamp,
-            deleted,
-        })
     }
 }
 
@@ -169,5 +177,17 @@ mod tests {
         let path = create_path();
         let timestamp = create_timestamp();
         Data::new(&path.join(timestamp.to_string() + ".data.sstable"))
+    }
+
+    #[test]
+    fn test_size_of_entry() {
+        let mut data = create_data().unwrap();
+        let entry = create_entry();
+        data.write(&entry).unwrap();
+        let offset = data.get_offset();
+        let entry_size = std::mem::size_of::<usize>() * 2 + 16 + 1 + 3 + 1;
+        println!("{}", std::mem::size_of::<usize>());
+        assert_ne!(offset, 0);
+        assert_eq!(offset, entry_size);
     }
 }
